624e87107a1d3e50ef7b6fb77a170e8b1ce3f8d7
Jerome Pratt <jerome.pratt@outlook.com>
2014-10-11 20:23:52 +0100

    refactored to 2 methods

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -5,21 +5,13 @@
   [x]
   (println x "Hello, World!"))
 
-(defn divisible-by-three? [number] 
-	(zero? (mod number 3))
-)
-
-(defn divisible-by-five? [number] 
-	(zero? (mod number 5))
-)
-
-(defn divisible-by-fifteen? [number]
-	(zero? (mod number 15))
+(defn divisible-by? [number divisor]
+	(zero? (mod number divisor))
 )
 
 (defn fizzbuzz [number]
-	(if(divisible-by-three? number) "fizz")
-	(if(divisible-by-five? number) "buzz")
-	(if(divisible-by-fifteen? number) "fizzbuzz")
+	(if(divisible-by? number 3) "fizz")
+	(if(divisible-by? number 5) "buzz")
+	(if(divisible-by? number 15) "fizzbuzz")
 	number
 )
--- test/fizzbuzz/core_test.clj
+++ test/fizzbuzz/core_test.clj
@@ -8,39 +8,39 @@
 
 (deftest is-divisible-by-three
 	(testing "it returns true for 3"
-		(is (= true (divisible-by-three? 3)))
+		(is (= true (divisible-by? 3 3)))
 
 	)
 )
 
 (deftest is-not-divisible-by-three
 	(testing "it returns false for 1"
-		(is (= false (divisible-by-three? 1)))
+		(is (= false (divisible-by? 1 3)))
 
 	)
 )
 
 (deftest is-divisible-by-five
 	(testing "it returns true for 5"
-		(is (= true (divisible-by-five? 5)))
+		(is (= true (divisible-by? 5 5)))
 	)
 )
 
 (deftest is-divisible-by-five
 	(testing "it returns false for 1"
-		(is (= false (divisible-by-five? 1)))
+		(is (= false (divisible-by? 1 5)))
 	)
 )
 
 (deftest is-divisible-by-fifteen
 	(testing "it returns true for 15"
-		(is (= true (divisible-by-fifteen? 15)))
+		(is (= true (divisible-by? 15 15)))
 	)
 )
 
 (deftest is-divisible-by-fifteen
 	(testing "it returns false for 1"
-		(is (= false (divisible-by-fifteen? 1)))
+		(is (= false (divisible-by? 1 15)))
 	)
 )
 
