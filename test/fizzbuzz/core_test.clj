95b6e410f10708e37a092de680e594f414e86f3e
Jerome Pratt <jerome.pratt@outlook.com>
2014-10-11 18:16:05 +0100

    first commit

---
+++ test/fizzbuzz/core_test.clj
@@ -0,0 +1,21 @@
+(ns fizzbuzz.core-test
+  (:require [clojure.test :refer :all]
+            [fizzbuzz.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
+
+(deftest is-divisible-by-three
+	(testing "it returns true for 3"
+		(is (= true (divisible-by-three? 3)))
+
+	)
+)
+
+(deftest is-not-divisible-by-three
+	(testing "it returns false for 1"
+		(is (= false (divisible-by-three? 1)))
+
+	)
+)
